7825588d4821249ac30224faf6d7de042713e29d
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-15 21:54:19 +0100

    add Task model and add simple logic to create task function

--- models/Task.js
+++ models/Task.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-// Setup the structure for the document (table)
+/* 
+  Setup the structure for the document (table) - avoid randomly added key:value pairs
+  Only properties specified in schema will be passed to database
+*/
 const TaskSchema = new mongoose.Schema({
   name: String,
   completed: Boolean,
 });
 
 // Setup a model - using the model we can get, update, create the documents
+// First argument is a name of collection for what model is used for
 module.exports = mongoose.model("Task", TaskSchema);
