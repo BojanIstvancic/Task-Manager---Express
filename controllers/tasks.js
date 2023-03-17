6e6c18bf4bb6cf8f82d3a105bf5423bad67e97b1
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-17 12:38:34 +0100

    add addTask and deleteTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,16 +18,40 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params; // set allias
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `There's no task with id ${taskID}` });
+    }
+
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(404).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
   res.send("Update a task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete a task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `There's no task with id ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+    // res.status(200).json({ task: null , status: 'success' });
+    // res.status(200).send();
+  } catch (error) {
+    res.status(404).json({ msg: error });
+  }
 };
 
 module.exports = {
