020192a0f643c4ad8c94c30554791a3c06e508e4
BojanIstvancic <bojan.istvancic@gmail.com>
2023-03-17 11:56:39 +0100

    add basic validation and complete get allTasks and createTask controller functions

--- models/Task.js
+++ models/Task.js
@@ -5,10 +5,20 @@ const mongoose = require("mongoose");
   Only properties specified in schema will be passed to database
 */
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "must porovide name"], // rquired property
+    trim: true, // trimm value
+    maxlength: [20, "name can not be more than 20 craracters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 // Setup a model - using the model we can get, update, create the documents
 // First argument is a name of collection for what model is used for
+
+// We can set validators and trims
 module.exports = mongoose.model("Task", TaskSchema);
